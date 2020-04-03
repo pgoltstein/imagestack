fce57cfddd362a114e27262b351ff8dab1f16dcd
Pieter Goltstein <pgoltstein@Pieters-MacBook-Pro.local>
2020-04-03 14:19:20 +0200

    basic functionality for handling meta data and loading arbitrary slices of image stacks directly from disk

--- scanimagestack/si_stack.py
+++ scanimagestack/si_stack.py
@@ -15,7 +15,10 @@ Created on Thu Jan 30, 2020
 # =============================================================================
 # Imports
 
+import os, glob
 import re
+import numpy as np
+from alive_progress import alive_bar
 from ScanImageTiffReader import ScanImageTiffReader
 import argparse
 
@@ -23,7 +26,7 @@ import argparse
 # =============================================================================
 # Arguments
 
-parser = argparse.ArgumentParser( description = "This module holds functions to import scanimage tiff files and gather the meta info stored in the headers. With thanks to Tobias Rose for some of the regular expressions. It can run as a test from the command line. \n (written by Pieter Goltstein - February 2020)")
+parser = argparse.ArgumentParser( description = "This module holds a dataclass calles imagestack and supporting functions. The main functionality is to handle imagestacks. Meta data can be accessed using simple dot-reference properties. Any slice of image data can be directly loaded from disk using Python's standard method of indexing slices directly on the imagestack class. Currently it supports stacks of scanimage tiff files only. With thanks to Tobias Rose for some of the regular expressions. The module can run from the command line for testing. \n (written by Pieter Goltstein - February 2020)")
 parser.add_argument('filepath', type=str, help= 'path to the tiff folder')
 parser.add_argument('filestem', type=str, help= 'filestem of tiffs')
 args = parser.parse_args()
@@ -33,7 +36,7 @@ args = parser.parse_args()
 # Functions
 
 def parseheader(header):
-    """ This function reads the most relevant information from the tiff header of a scanimage tiff fileself.
+    """ This function reads the most relevant information from the tiff header of a scanimage tiff.
         Inputs
         - header: The can be obtained from ScanImageTiffReader, header (tifffile.description(0))
         Returns
@@ -43,21 +46,21 @@ def parseheader(header):
     # Define the regular expressions needed to extract the information
     rx_dict = {
         "stackNumSlices": re.compile(r'stackNumSlices = (?P<stackNumSlices>\d+)'),
-        "scanZoomFactor": re.compile(r'scanZoomFactor = (?P<scanZoomFactor>\d+.\d+)'),
-        "scanFrameRate": re.compile(r'scanFrameRate = (?P<scanFrameRate>\d+.\d+)'),
+        "scanZoomFactor": re.compile(r'scanZoomFactor = (?P<scanZoomFactor>\d+.?\d*)'),
+        "scanFrameRate": re.compile(r'scanFrameRate = (?P<scanFrameRate>\d+.?\d*)'),
         "channelsSave": re.compile(r'channelsSave = (?P<channelsSave>\d+)'),
         "fastZNumVolumes": re.compile(r'fastZNumVolumes = (?P<fastZNumVolumes>\d+)'),
         "triggerClockTimeFirst": re.compile(r'triggerClockTimeFirst = (?P<triggerClockTimeFirst>\'\d+-\d+-\d+ \d+:\d+:\d+.\d+\')'),
         "loggingFramesPerFile": re.compile(r'loggingFramesPerFile = (?P<loggingFramesPerFile>\d+)'),
-        "beamPowers": re.compile(r'beamPowers = (?P<beamPowers>\d+.\d+)'),
+        "beamPowers": re.compile(r'beamPowers = (?P<beamPowers>\d+.?\d*)'),
         "loggingFileStem": re.compile(r'loggingFileStem = (?P<loggingFileStem>\'.+\')'),
         "motorPosition": re.compile(r'motorPosition = (?P<motorPosition>\[.+\])'),
         "pmtGain": re.compile(r'pmtGain = (?P<pmtGain>\[.+\])'),
         "scanLinesPerFrame": re.compile(r'scanLinesPerFrame = (?P<scanLinesPerFrame>\d+)'),
         "scanPixelsPerLine": re.compile(r'scanPixelsPerLine = (?P<scanPixelsPerLine>\d+)'),
-        "stackZEndPos": re.compile(r'stackZEndPos = (?P<stackZEndPos>\d+.\d+)'),
-        "stackZStartPos": re.compile(r'stackZStartPos = (?P<stackZStartPos>\d+.\d+)'),
-        "stackZStepSize": re.compile(r'stackZStepSize = (?P<stackZStepSize>\d+.\d+)'),
+        "stackZEndPos": re.compile(r'stackZEndPos = (?P<stackZEndPos>\d+.?\d*)'),
+        "stackZStartPos": re.compile(r'stackZStartPos = (?P<stackZStartPos>\d+.?\d*)'),
+        "stackZStepSize": re.compile(r'stackZStepSize = (?P<stackZStepSize>\d+.?\d*)'),
     }
 
     # Now step through the dictionary and extract the information as int, float, string or a list of floats
@@ -93,26 +96,152 @@ def parseheader(header):
 # Classes
 
 class imagestack(object):
-    """ This class holds the data of an entire (multi-tiff) scan image tiff stack, including methods for loading a single image, a slice, an enture image stack or just for accessing the meta data
+    """ This class represents an entire (multi-tiff) scanimage stack. Image channel and image plane should be set manually (defaults are 0).
+
+        The class can load the image data using standard np.ndarray indexing:
+         * data = imagestack[:] returns all the data
+         * data = imagestack[1] returns the second frame (zero based slice)
+         * data = imagestack[[5,8,10]] returns frames 5,8 and 10
+         * data = imagestack[::2] returns every second frame.
+
+         In addition, the class has several methods for accessing the meta data, which can be accessed as properties. For instance:
+         * res = imagestack.resolution returns the [y,x] image resolution
+         * nchannels = imagestack.nchannels returns number of image channels
     """
-    def __init__(self, filestem=None, filepath=None):
+
+    def __init__(self, filestem='', filepath=None, extention="tif"):
         """ Initializes the image stack and gathers the meta data
             Inputs
             - filestem: Part of the file name that is shared among all tiffs belonging to the stack (optional, if left out all tiffs in filepath will be included)
             - filepath: Full directory path to the tiffs
+            - extention: file extention of the stack
         """
 
+        # Set the filepath
+        if filepath is not None:
+            self.filepath = filepath
+        else:
+            self.filepath = os.getcwd()
 
+        # Find the tiff files
+        self.block_files = sorted( glob.glob( os.path.join( self.filepath, filestem+'*.'+extention ) ) )
+        self._nblocks = len(self.block_files)
+        print("Imagestack of {} {} files, using filestem {}".format( self._nblocks, extention, filestem+'*.'+extention))
+
+        # Load and parse the header
+        with ScanImageTiffReader( self.block_files[0] ) as tifffile:
+            header = (tifffile.description(0))
+            self.si_info = parseheader(header)
+        self._nframesperblock = self.si_info["loggingFramesPerFile"]
+        print("{} frames, {} planes, {} channels, {} x {} pixels".format( self.nframes, self.nplanes, self.nchannels, self.yres, self.xres ))
+
+        # Default settings of internal variables
+        self._datatype = np.int16
+        self._channel = 0
+        self._plane = 0
+
+    @property
+    def xres(self):
+        """ Number of pixels along the x-axis """
+        return self.si_info["scanPixelsPerLine"]
+
+    @property
+    def yres(self):
+        """ Number of pixels along the y-axis """
+        return self.si_info["scanLinesPerFrame"]
+
+    @property
+    def resolution(self):
+        """ Number of pixels along the y- and x-axis """
+        return self.si_info["scanLinesPerFrame"], self.si_info["scanPixelsPerLine"]
+
+    @property
+    def nframes(self):
+        """ Number of frames per slice and channel """
+        return self.si_info["fastZNumVolumes"]
+
+    @property
+    def nplanes(self):
+        """ Number of planes """
+        return self.si_info["stackNumSlices"]
+
+    @property
+    def nchannels(self):
+        """ Number of channels """
+        return self.si_info["channelsSave"]
+
+    @property
+    def channel(self):
+        """ Returns the currently selected channel """
+        return self._channel
+
+    @channel.setter
+    def channel(self,chan_nr):
+        """ Sets the channel """
+        self._channel = int(chan_nr)
+
+    @property
+    def plane(self):
+        """ Returns the currently selected image plane """
+        return self._plane
+
+    @plane.setter
+    def plane(self,plane_nr):
+        """ Sets the plane """
+        self._plane = int(plane_nr)
+
+    def __getitem__(self, indices):
+        """ Loads and returns the image data directly from disk """
+
+        # Use the provided slice object to get the requested frames
+        if isinstance(indices, slice):
+            frames = np.arange(self.nframes)[indices]
+        elif isinstance(indices, list) or isinstance(indices, tuple):
+            frames = np.array(indices)
+        else:
+            frames = np.array([indices,])
+
+        # Define the indices of the requested frames
+        # tiffs are stored as [ch0-sl0, ch1-sl0, ch0-sl1, ch2-sl1, ch0-sl2 etc]
+        start_frame = (self._plane * self.nchannels) + self._channel
+        frame_jump = self.nchannels * self.nplanes
+        frame_ixs = frames * frame_jump
+        n_frame_ixs = len(frame_ixs)
+        frame_ids = np.arange(n_frame_ixs)
+
+        # Identify the block files to open, and which frames to load
+        block_ixs_per_frame = np.floor(frame_ixs / self._nframesperblock).astype(np.int)
+        frame_ixs_in_block = np.mod(frame_ixs, self._nframesperblock)
+        block_numbers,block_inverse = np.unique(block_ixs_per_frame, return_inverse=True)
+        block_indexes = list(range(len(block_numbers)))
+
+        # Split the list of frames into a per-block list
+        frame_ixs_per_block = []
+        frame_ids_per_block = []
+        for b in block_indexes:
+            frame_ixs_per_block.append( list(frame_ixs_in_block[block_inverse==b]) )
+            frame_ids_per_block.append( list(frame_ids[block_inverse==b]) )
+
+        # Loop block files, and frame indices to load all requested frames
+        imagedata = np.zeros((self.yres,self.xres,n_frame_ixs),dtype=self._datatype)
+        with alive_bar(n_frame_ixs) as bar:
+            for bnr,bix in zip(block_numbers,block_indexes):
+                with ScanImageTiffReader(self.block_files[bnr]) as tifffile:
+                    for ix,id_ in zip( frame_ixs_per_block[bix], frame_ids_per_block[bix] ):
+                        imagedata[:,:,id_] = tifffile.data(beg=ix,end=ix+1)
+                        bar()
+
+        # Return the stack
+        return imagedata
 
 # =============================================================================
 # Main, for testing from command line
 
 if __name__ == '__main__':
-    with ScanImageTiffReader( "/Users/pgoltstein/data/odmapping/O03/20200218-OD-L23-L4-L5/410/O03_20200218_005_001.tif" ) as tifffile:
-        print("Loaded: /Users/pgoltstein/data/odmapping/O03/20200218-OD-L23-L4-L5/410/O03_20200218_005_001.tif")
-        header = (tifffile.description(0))
-        dimensions = (tifffile.shape())
-        print("Image stack dimensions: {}".format(dimensions))
-        si_info = parseheader(header)
-        for k,v in si_info.items():
-            print("    - {}: {}".format(k,v))
+    im = imagestack(filestem=args.filestem, filepath=args.filepath, extention="tif")
+    a=im[[3,4,5]]
+    # a=im[3]
+    # a=im[:100]
+    # a=im[-100:]
+    # a=im[-200:-100:2]
+    # a=im[::25]
