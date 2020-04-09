828b21cf22237b5b2f9adfb98be6f5772ed5bc17
Pieter Goltstein <pgoltstein@Pieters-MacBook-Pro.local>
2020-04-09 20:12:16 +0200

    tested and changed requirements

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@
 from setuptools import setup
 
 setup(  name='scanimagestack',
-        version='0.01',
+        version='0.11',
         description='Contains functions to handle complete scanImage stacks (consisting of multiple tiff blocks)',
         url='https://github.com/pgoltstein/scanimagestack',
         author='Pieter Goltstein',
         author_email='xpieter@mac.com',
         license='GNU GENERAL PUBLIC LICENSE Version 3',
         packages=['scanimagestack'],
-        install_requires=['re','ScanImageTiffReader'],
+        install_requires=['numpy','scanimage-tiff-reader','alive_progress'],
         zip_safe=False
         )
