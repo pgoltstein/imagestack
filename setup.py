785944e88faa4414aabd83fd5657c57bebc49a57
Pieter Goltstein <pgoltstein@Pieters-MacBook-Pro.local>
2020-04-09 16:24:54 +0200

    reorganized setup file

--- setup.py
+++ setup.py
@@ -3,4 +3,14 @@
 
 from setuptools import setup
 
-setup( name='scanimagestack', version='0.01', description='Contains functions to handle complete scanImage stacks (consisting of multiple tiff blocks)', url='https://github.com/pgoltstein/scanimagestack', author='Pieter Goltstein', author_email='xpieter@mac.com', license='GNU GENERAL PUBLIC LICENSE Version 3', packages=['scanimagestack'], install_requires=['re','ScanImageTiffReader'], zip_safe=False )
+setup(  name='scanimagestack',
+        version='0.01',
+        description='Contains functions to handle complete scanImage stacks (consisting of multiple tiff blocks)',
+        url='https://github.com/pgoltstein/scanimagestack',
+        author='Pieter Goltstein',
+        author_email='xpieter@mac.com',
+        license='GNU GENERAL PUBLIC LICENSE Version 3',
+        packages=['scanimagestack'],
+        install_requires=['re','ScanImageTiffReader'],
+        zip_safe=False
+        )
